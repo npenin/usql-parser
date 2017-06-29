a56cfe7ccd056767d4225190f69123a8426d4a6e
Anirudh Rayabharam <mail@anirudhrb.com>
2017-06-29 20:40:54 +0530

    Intial commit

---
+++ USqlParser/Grammar/USql.g4
@@ -0,0 +1,307 @@
+﻿ grammar USql;
+
+
+/*
+ * Parser rules
+ */
+
+prog
+	: createDatabaseStatement prog
+	| createManagedTableWithSchemaStatement prog
+	| alterTableStatement prog
+	| alterTableAddDropPartitionStatement prog
+	| dropTableStatement prog
+	| createSchemaStatement prog
+	| declareVariableStatement prog
+	| useDatabaseStatement prog
+	| insertStatement prog
+    | EOF
+	;
+
+createDatabaseStatement
+	: CREATE DATABASE ( IF NOT EXISTS )? dbName ';'
+	;
+
+dbName
+	: quotedOrUnquotedIdentifier
+	;
+
+unquotedIdentifier
+	: ID
+	;
+
+quotedIdentifier
+	: '[' unquotedIdentifier ']'
+	;
+
+quotedOrUnquotedIdentifier
+	: quotedIdentifier
+	| unquotedIdentifier
+	;
+
+multipartIdentifier
+	: quotedOrUnquotedIdentifier
+	| quotedOrUnquotedIdentifier '.' quotedOrUnquotedIdentifier
+	| quotedOrUnquotedIdentifier '.' quotedOrUnquotedIdentifier '.' quotedOrUnquotedIdentifier
+	;
+
+useDatabaseStatement
+	: 'USE' DATABASE dbName ';'
+	;
+
+numericType
+	: NumericTypeNonNullable
+	| NumericTypeNonNullable '?'
+	;
+
+simpleType
+	: numericType
+	| TextualType
+	| TemporalType
+	| OtherType
+	;
+
+builtInType
+	: simpleType
+	;
+
+createSchemaStatement
+	: CREATE SCHEMA ( IF NOT EXISTS )? quotedOrUnquotedIdentifier ';'
+	;
+
+columnDefinition
+	: quotedOrUnquotedIdentifier builtInType
+	;
+
+tableWithSchema
+	: ( '(' ( columnDefinition ',' )*
+		( tableIndex partitionSpecification )?
+		  ( ',' columnDefinition )* ')' )
+	| ( '(' ( columnDefinition ',' )*
+		( tableIndex )
+			( ',' columnDefinition )* ')' partitionSpecification )
+	;
+
+tableName
+	: multipartIdentifier
+	;
+
+createManagedTableWithSchemaStatement
+	: CREATE TABLE ( IF NOT EXISTS )? tableName tableWithSchema ';'
+	;
+
+sortDirection
+	: 'ASC' | 'DESC'
+	;
+
+sortItem
+	: quotedOrUnquotedIdentifier ( sortDirection )?
+	;
+
+sortItemList
+	: sortItem ( ',' sortItem )*
+	;
+
+tableIndex
+	: INDEX quotedOrUnquotedIdentifier CLUSTERED '(' sortItemList ')'
+	;
+
+identifierList
+	: quotedOrUnquotedIdentifier ( ',' quotedOrUnquotedIdentifier )*
+	;
+
+distributionScheme
+	: 'RANGE' '(' sortItemList ')'
+	| 'HASH' '(' identifierList ')'
+	| 'DIRECT' 'HASH' '(' quotedOrUnquotedIdentifier ')'
+	| 'ROUND' 'ROBIN'
+	;
+
+distributionSpecification
+	: 'DISTRIBUTED' ( 'BY' )? distributionScheme
+	;
+
+partitionSpecification
+	: ( 'PARTITIONED' ( 'BY' )? '(' identifierList ')' )? distributionSpecification
+	;
+
+columnDefinitionList
+	: '(' columnDefinition ( ',' columnDefinition )* ')'
+	;
+
+alterTableStatement
+	: 'ALTER' 'TABLE' multipartIdentifier
+	  ( 'REBUILD' 
+	  | ADD 'COLUMN' columnDefinitionList
+	  | DROP 'COLUMN' identifierList ) ';'
+	;
+
+systemVariable
+	: '@@' unquotedIdentifier
+	;
+
+userVariable
+	: '@' unquotedIdentifier
+	;
+
+variable
+	: systemVariable
+	| userVariable
+	;
+
+declareVariableStatement
+	: 'DECLARE' variable builtInType '=' ~( ';' )* ';'
+	; 
+
+/*
+ * TODO: Include StaticVariable and BinaryLiteral. Need to figure it out.
+ */
+staticExpression
+	: StringLiteral
+	| CharLiteral
+	| NumberLiteral
+	| userVariable
+	;
+
+staticExpressionList
+	: staticExpression ( ',' staticExpression )*
+	;
+
+staticExpressionRowConstructor
+	: '(' staticExpressionList ')'
+	;
+
+partitionLabel
+	: 'PARTITION' staticExpressionRowConstructor
+	;
+
+partitionLabelList
+	: partitionLabel ( ',' partitionLabel )*
+	;
+
+alterTableAddDropPartitionStatement
+	: 'ALTER' 'TABLE' multipartIdentifier
+	   ( ADD ( IF NOT EXISTS )? | DROP ( IF NOT EXISTS )? ) partitionLabelList ';'
+	;
+
+dropTableStatement
+	: DROP TABLE ( IF EXISTS )? multipartIdentifier ';'
+	;
+
+integrityViolationAction
+	: 'IGNORE'
+	| 'MOVE' 'TO' partitionLabel
+	;
+
+integrityClause
+	: 'ON' 'INTEGRITY' 'VIOLATION' integrityViolationAction
+	;
+
+rowConstructorList
+	: '(' ~( ',' )* ( ',' ~( ',' )* )* ')'
+	;
+
+tableValueConstructorExpression
+	: 'VALUES' rowConstructorList
+	;
+
+insertSource
+	: tableValueConstructorExpression
+	;
+
+insertStatement
+	: 'INSERT' 'INTO' multipartIdentifier ( '(' identifierList ')' )?
+	  ( partitionLabel | integrityClause )? insertSource ';'
+	;
+
+/*
+ * Lexer rules
+ * TODO: make upper case
+ */
+
+CREATE
+	: 'create' | 'CREATE'
+	;
+
+DATABASE
+	: 'database' | 'DATABASE'
+	;
+
+SCHEMA
+	: 'SCHEMA'
+	;
+
+TABLE
+	: 'TABLE'
+	;
+
+INDEX
+	: 'INDEX'
+	;
+
+CLUSTERED
+	: 'CLUSTERED'
+	;
+
+IF
+	: 'if' | 'IF'
+	;
+
+NOT
+	: 'not' | 'NOT'
+	;
+
+EXISTS
+	: 'exists' | 'EXISTS'
+	;
+
+ADD
+	: 'ADD'
+	;
+
+DROP
+	: 'DROP'
+	;
+
+NumericTypeNonNullable
+	: 'byte' | 'sbyte' | 'int' | 'uint' | 'long' | 'ulong' | 'float' | 'double' | 'decimal' | 'short' | 'ushort'
+	;
+
+TextualType
+	: 'char' | 'char?' | 'string'
+	;
+
+TemporalType
+	: 'DateTime' | 'DateTime?'
+	;
+
+OtherType
+	: 'bool' | 'bool?' | 'Guid' | 'Guid?' | 'byte[]'
+	;
+
+CharLiteral
+	: '\'' . '\''
+	;
+
+StringLiteral
+	: '"' ( 'a' .. 'z' | 'A' .. 'Z' | '_' )+ '"'
+	;
+
+/*
+ * TODO: Improve the number literal rule to include floats, hex, octal, long etc. More research needed.
+ */
+NumberLiteral
+	: ( '0' .. '9' )+
+	;
+
+NEWLINE
+   : '\r'? '\n' -> skip
+   ;
+
+WS
+   : ( ' ' | '\t' | '\n' | '\r' )+ -> skip
+   ;
+
+ID
+   : ( 'a' .. 'z' | 'A' .. 'Z' | '_' )+
+   ;
---
+++ USqlParserDriver/Program.cs
@@ -0,0 +1,155 @@
+﻿using AnirudhRb.USqlParser;
+using Antlr4.Runtime;
+using System;
+using System.IO;
+using Antlr4.Runtime.Misc;
+
+namespace USqlParserDriver
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stream inputStream = Console.OpenStandardInput();
+            AntlrInputStream input = new AntlrInputStream(inputStream);
+            USqlLexer uSqlLexer = new USqlLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(uSqlLexer);
+            USqlParser parser = new USqlParser(tokens);
+            parser.ErrorHandler = new BailErrorStrategy();
+            parser.AddErrorListener(new ParserErrorListener());
+            parser.AddParseListener(new FullListener());
+            try
+            {
+                parser.prog();
+            }
+            catch (ParseCanceledException e)
+            {
+                Console.WriteLine("Offending token: " + ((InputMismatchException)e.InnerException).OffendingToken.Text);
+                throw e;
+            }
+        }
+    }
+
+    public class LexerErrorListener : IAntlrErrorListener<int>
+    {
+        public void SyntaxError(IRecognizer recognizer, int offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+        {
+            Console.WriteLine("Error in parser at line {0}:{1}", line, charPositionInLine);
+            Console.WriteLine("offender: " + e.OffendingToken.Text);
+            throw new Exception("Parsing failed. " + e.Message);
+        }
+    }
+
+    public class ParserErrorListener : IAntlrErrorListener<IToken>
+    {
+        public void SyntaxError(IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+        {
+            Console.WriteLine("Error in parser at line {0}:{1}", line, charPositionInLine);
+            Console.WriteLine("offender. " + e.OffendingToken.Text);
+            throw new Exception("Parsing failed. Reason: " + e.Message);
+        }
+    }
+
+    public class FullListener : USqlBaseListener
+    {
+        public override void ExitUseDatabaseStatement([NotNull] USqlParser.UseDatabaseStatementContext context)
+        {
+            base.ExitUseDatabaseStatement(context);
+            Console.WriteLine("USE statement. Changing database context to: {0}", context.dbName().GetText());
+        }
+
+        public override void ExitCreateSchemaStatement([NotNull] USqlParser.CreateSchemaStatementContext context)
+        {
+            base.ExitCreateSchemaStatement(context);
+            Console.WriteLine("Create schema: {0}", context.quotedOrUnquotedIdentifier().GetText());
+        }
+
+        public override void ExitDropTableStatement([NotNull] USqlParser.DropTableStatementContext context)
+        {
+            base.ExitDropTableStatement(context);
+            Console.WriteLine("Drop table {0} {1}", (context.IF() == null) ? "" : "IF EXISTS", context.multipartIdentifier().GetText());
+        }
+
+        public override void ExitCreateDatabaseStatement([NotNull] USqlParser.CreateDatabaseStatementContext context)
+        {
+            base.ExitCreateDatabaseStatement(context);
+            if (context.exception != null)
+            {
+                Console.WriteLine("exception: " + context.exception.Message);
+            }
+            else
+            {
+                Console.WriteLine("Encountered Create Database statement for Db {0}", context.dbName().GetText());
+            }
+        }
+
+        public override void ExitCreateManagedTableWithSchemaStatement([NotNull] USqlParser.CreateManagedTableWithSchemaStatementContext context)
+        {
+            base.EnterCreateManagedTableWithSchemaStatement(context);
+
+            Console.WriteLine("Creating table: {0}", context.tableName().GetText());
+            foreach (var columnDef in context.tableWithSchema().columnDefinition())
+            {
+                Console.WriteLine("    " + columnDef.quotedOrUnquotedIdentifier().GetText() + " " + columnDef.builtInType().GetText());
+            }
+
+            Console.Write("Index {0} on ", context.tableWithSchema().tableIndex().quotedOrUnquotedIdentifier().GetText());
+            foreach (var x in context.tableWithSchema().tableIndex().sortItemList().sortItem())
+            {
+                Console.Write("{0} {1} ", x.quotedOrUnquotedIdentifier().GetText(), (x.sortDirection() == null) ? "Default direction" : x.sortDirection().GetText());
+            }
+            Console.WriteLine();
+
+            Console.Write("Partitioned by: ");
+            foreach (var x in context.tableWithSchema().partitionSpecification().identifierList().quotedOrUnquotedIdentifier())
+            {
+                Console.Write("{0}, ", x.GetText());
+            }
+            Console.WriteLine("\b\b");
+
+            Console.Write("Distributed by: ");
+            foreach (var x in context.tableWithSchema().partitionSpecification().distributionSpecification().distributionScheme().identifierList().quotedOrUnquotedIdentifier())
+            {
+                Console.Write("{0}, ", x.GetText());
+            }
+            Console.WriteLine("\b\b");
+            Console.WriteLine("Create table done");
+        }
+
+        public override void ExitAlterTableStatement([NotNull] USqlParser.AlterTableStatementContext context)
+        {
+            base.ExitAlterTableStatement(context);
+            Console.WriteLine("Altering table: " + context.multipartIdentifier().GetText() + " 1: " + context.multipartIdentifier().quotedOrUnquotedIdentifier(2).GetText());
+        }
+
+        public override void ExitAlterTableAddDropPartitionStatement([NotNull] USqlParser.AlterTableAddDropPartitionStatementContext context)
+        {
+            base.EnterAlterTableAddDropPartitionStatement(context);
+            Console.Write("Altering table with partition: " + context.multipartIdentifier().GetText());
+            Console.Write("Operation: {0}. ", (context.ADD() == null) ? "DROP" : "ADD");
+            Console.Write("Partitions: ");
+            foreach (var partitionLabel in context.partitionLabelList().partitionLabel())
+            {
+                Console.Write("(");
+                foreach (var staticExpression in partitionLabel.staticExpressionRowConstructor().staticExpressionList().staticExpression())
+                {
+                    Console.Write(staticExpression.GetText());
+                }
+                Console.Write(") ");
+            }
+            Console.WriteLine();
+        }
+
+        public override void ExitDeclareVariableStatement([NotNull] USqlParser.DeclareVariableStatementContext context)
+        {
+            base.ExitDeclareVariableStatement(context);
+            Console.WriteLine("Declaring variable: " + context.variable().GetText());
+        }
+
+        public override void ExitInsertStatement([NotNull] USqlParser.InsertStatementContext context)
+        {
+            base.ExitInsertStatement(context);
+            Console.WriteLine("Inserting into {0}, values {1}", context.multipartIdentifier().GetText(), context.insertSource().GetText());
+        }
+    }
+}
